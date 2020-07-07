b101f4300540abcab23aba669e5f5a0aae340338
Alef oliveira <you@example.com>
2020-07-06 22:40:20 -0300

    adc njk

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const receitas = require('./data');
+
+const server = express();
+
+server.use(express.static('public'))
+server.set('view engine', 'njk')
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get('/', (req,res)=>{
+    return res.render('index',  { items: receitas})
+})
+
+server.get('/about', (req,res)=>{
+    return res.render('about')
+})
+
+server.get('/receitas', (req,res)=>{
+    return res.render('receitas', { items: receitas})
+})
+
+
+server.get("/receita", function (req, res) {
+   
+    const id = req.query.id
+    const receita = receitas.find(function(receita){
+       if (receita.id == id) {
+           return true;
+       }
+    })
+    if (!receita){
+        res.send('Not found')
+    }
+   
+    return res.render('receita', {item: receita})
+
+})
+
+server.use(function(req, res) {
+    res.status(404).send("not-found");
+});
+
+
+server.listen(5000, ()=>{
+    console.log('server is runing');
+})
